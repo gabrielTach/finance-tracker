977b99af638dcf19734bee971f9ae0c259755dfd
Jan Taverniers <tachfur@gmail.com>
2020-08-30 09:28:11 +0200

    feat: limits number of stocks tracked, and adds remove action.

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -12,4 +12,12 @@ class UserStocksController < ApplicationController
 
     redirect_to my_portofolio_path
   end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stock.destroy
+    flash[:notice] = "#{stock.ticker} was successfully removed from portofolio"
+    redirect_to my_portofolio_path
+  end
 end
