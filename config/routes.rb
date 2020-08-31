3691add8604a1cef93456e5417bee72a1c3edf45
Jan Taverniers <tachfur@gmail.com>
2020-08-31 17:13:27 +0200

    feat: adds friends

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
   get 'my_portofolio', to: 'users#my_portofolio'
+  get 'my_friends', to: 'users#my_friends'
   get 'search_stock', to: 'stocks#search'
 end
